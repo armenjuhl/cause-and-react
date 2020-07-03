08360055415b1ccf4a91ede43759f2bc1942e43c
Armen Juhl <armenjuhl@gmail.com>
2020-07-03 13:37:34 -0700

    PRE-REDUX APP: form utilizes local state. Custom functions to handle events

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,50 +1,47 @@
 import React, {Component} from 'react';
 import '../App.css';
 import Menu from '../components/MenuComponent';
-import {DISHES} from '../shared/dishes';
 import Header from '../components/HeaderComponent';
 import Footer from '../components/FooterComponent';
 import Home from '../components/HomeComponent';
 import DishDetail from '../components/DishDetailComponent';
 import Contact from '../components/ContactComponent';
 import About from '../components/AboutComponent';
-import {COMMENTS} from '../shared/comments';
-import {PROMOTIONS} from '../shared/promotions';
-import {LEADERS} from '../shared/leaders';
-import {Redirect, Route, Switch} from 'react-router-dom';
+import {Redirect, Route, Switch, withRouter} from 'react-router-dom';
+import {connect} from 'react-redux';
 
+const mapStateToProps = state => {
+  return {
+    dishes: state.dishes,
+    comments: state.comments,
+    promotions: state.promotions,
+    leaders: state.leaders
+  }
+};
 
 class Main extends Component {
 
   constructor(props) {
     super(props);
-
-    this.state = {
-      dishes: DISHES,
-      comments: COMMENTS,
-      promotions: PROMOTIONS,
-      leaders: LEADERS
-    };
   }
-
 
   render() {
 
     const HomePage = () => {
       return (
           <Home
-              dish={this.state.dishes.filter((dish) => dish.featured)[0]}
-              promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
-              leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+              dish={this.props.dishes.filter((dish) => dish.featured)[0]}
+              promotion={this.props.promotions.filter((promo) => promo.featured)[0]}
+              leader={this.props.leaders.filter((leader) => leader.featured)[0]}
           />
       );
     };
 
     const DishWithId = ({match}) => {
       return (
-          <DishDetail dish={this.state.dishes.filter((dish) => dish.id ===
+          <DishDetail dish={this.props.dishes.filter((dish) => dish.id ===
               parseInt(match.params.dishId, 10))[0]}
-                      comments={this.state.comments.filter((comment) => comment.dishId ===
+                      comments={this.props.comments.filter((comment) => comment.dishId ===
                           parseInt(match.params.dishId, 10))}/>
       );
     };
@@ -55,8 +52,8 @@ class Main extends Component {
           <Switch>
             <Route path="/home" component={HomePage}/>
             {/* If I need to pass a component props use Route format below */}
-            <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes}/>}/>
-            <Route exact path="/aboutus" component={() => <About leaders={this.state.leaders}/>}/>
+            <Route exact path="/menu" component={() => <Menu dishes={this.props.dishes}/>}/>
+            <Route exact path="/aboutus" component={() => <About leaders={this.props.leaders}/>}/>
             <Route path="/menu/:dishId" component={DishWithId}/>
             <Route exact path="/contactus" component={Contact}/>
             <Redirect to="/home"/>
@@ -67,5 +64,4 @@ class Main extends Component {
   }
 }
 
-
-export default Main;
+export default withRouter(connect(mapStateToProps)(Main));

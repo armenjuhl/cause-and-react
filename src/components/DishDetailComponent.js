46093d8ae540579043afe45582d1d954b80454d1
Armen Juhl <armenjuhl@gmail.com>
2020-07-09 15:22:58 -0700

    combine redux reducers

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -1,17 +1,6 @@
 import React, {Component} from 'react';
 import {
-  Card,
-  CardImg,
-  CardBody,
-  CardTitle,
-  CardText,
-  Breadcrumb,
-  BreadcrumbItem,
-  Button,
-  Modal,
-  ModalBody,
-  ModalHeader,
-  Label
+  Card, CardImg, CardBody, CardTitle, CardText, Breadcrumb, BreadcrumbItem, Button, Modal, ModalBody, ModalHeader, Label
 } from 'reactstrap';
 import {Control, LocalForm, Errors} from 'react-redux-form';
 import {Link} from 'react-router-dom';
@@ -172,7 +161,7 @@ const DishDetail = (props) => {
             </div>
             <div className="col-12 col-md-5 m-1">
               <RenderComments comments={props.comments}/>
-              <CommentForm />
+              <CommentForm/>
             </div>
           </div>
         </div>

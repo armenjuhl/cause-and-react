675e078b3aa6a51479d6ea1e85d97577ef769213
Armen Juhl <armenjuhl@gmail.com>
2020-07-28 14:42:13 -0700

    add json-server. upload build folder into json-server public

---
+++ json-server/public/precache-manifest.72eba24c3391ed2c66bad2091a723943.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcfa3873ea1e8bfa83befa96508be395",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9c8dcba0064d417c185b",
+    "url": "/static/css/2.7f0a60d8.chunk.css"
+  },
+  {
+    "revision": "6cc2447117ccc4c53436",
+    "url": "/static/css/main.445b222f.chunk.css"
+  },
+  {
+    "revision": "9c8dcba0064d417c185b",
+    "url": "/static/js/2.7c80acaf.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/static/js/2.7c80acaf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6cc2447117ccc4c53436",
+    "url": "/static/js/main.0b8e7522.chunk.js"
+  },
+  {
+    "revision": "0b251c8e985260961465",
+    "url": "/static/js/runtime-main.59f1edc0.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
